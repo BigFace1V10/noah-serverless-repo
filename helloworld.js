48c76ad661573b03943e545b792413e1903788b9
Noah Zhang <zhangh21@usc.edu>
2022-05-26 08:38:15 -0400

    hello world !

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,11 @@
 // code your `hello` function here!
+
+function hello() 
+{
+    return "Hello World";
+}
+
+hello.exports = hello;
+
+// let hello = require('helloworld.js')
+// let output = hello.hello()
