26a9e6af49e8cdae851210b9535534f9da67fcf8
Haoyang Zhang <zhangh21@usc.edu>
2022-06-08 15:29:07 -0400

    late 01

--- late.js
+++ late.js
@@ -1,1 +1,17 @@
 // start coding your function here!
+
+function running_late(date)
+{
+    let dateArr = date.split(' ')
+    let hour = dateArr[3].slice(0, 2)
+    if (hour >= 22)
+    {
+        return "It is late!";
+    }
+    else
+    {
+        return "It is still early!";
+    }
+}
+
+exports.running_late = running_late;
